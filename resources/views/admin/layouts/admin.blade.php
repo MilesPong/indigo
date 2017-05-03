d0b66928ba35c2465a9c0923e06643bf3d24662f
Miles Pong <mingpeng16@gmail.com>
2017-05-03 17:37:30 +0800

    Modify auth views to adjust AdminLTE

--- resources/views/admin/layouts/admin.blade.php
+++ resources/views/admin/layouts/admin.blade.php
@@ -4,45 +4,9 @@ This is a starter template page. Use this page to start your new project from
 scratch. This page gets rid of all links and provides the needed markup only.
 -->
 <html lang="{{ config('app.locale') }}">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>{{ config('app.name', 'Laravel') }} - Dashboard</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-  <!-- CSRF Token -->
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+@include('admin.partials.html-header')
 
-  <!-- Bootstrap -->
-  <!-- Font Awesome -->
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-  <!-- Theme style -->
-  <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
-        page. However, you can choose any other skin. Make sure you
-        apply the skin class to the body tag so the changes take effect.
-  -->
-  <link rel="stylesheet" href="{{ mix('css/adminlte.css') }}">
-
-
-  <!-- Custom CSS -->
-  @stack('css')
-
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
-
-  <script>
-      window.Laravel = {!! json_encode([
-          'csrfToken' => csrf_token(),
-      ]) !!};
-  </script>
-
-</head>
 <!--
 BODY TAG OPTIONS:
 =================
@@ -93,19 +57,7 @@ desired effect
 </div>
 <!-- ./wrapper -->
 
-<!-- REQUIRED JS SCRIPTS -->
+@include('admin.partials.scripts')
 
-<!-- jQuery 2.2.3 -->
-<!-- Bootstrap 3.3.6 -->
-<!-- AdminLTE App -->
-<script src="{{ mix('js/app.js') }}"></script>
-
-<!-- Custom JS -->
-@stack('js')
-
-<!-- Optionally, you can add Slimscroll and FastClick plugins.
-     Both of these plugins are recommended to enhance the
-     user experience. Slimscroll is required when using the
-     fixed layout. -->
 </body>
 </html>
