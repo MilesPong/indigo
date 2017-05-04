f04bc00482f707542c79b6246cbd997e0bbf5471
Miles Pong <mingpeng16@gmail.com>
2017-05-04 11:22:18 +0800

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,11 +13,7 @@ const { mix } = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
-   .less('resources/assets/less/adminlte.less', 'public/css').options({
-   		processCssUrls: false
-   })
-   .copyDirectory('node_modules/font-awesome/fonts/','public/fonts/')
-   .copyDirectory('node_modules/ionicons/dist/fonts/','public/fonts/')
+   .less('resources/assets/less/adminlte.less', 'public/css')
    .copyDirectory('node_modules/admin-lte/plugins/', 'public/plugins/');
 
 if (!mix.config.inProduction) {
