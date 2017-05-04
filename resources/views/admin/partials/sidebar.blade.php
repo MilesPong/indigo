adab1bfe4d87379a044bdb571d7f8814ecb210d6
Miles Pong <mingpeng16@gmail.com>
2017-05-04 17:35:42 +0800

    Update sidebar content

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -32,19 +32,21 @@
     <ul class="sidebar-menu">
       <li class="header">HEADER</li>
       <!-- Optionally, you can add icons to the links -->
-      <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
-      <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
+      <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Home</span></a></li>
       <li class="treeview">
-        <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
-          <span class="pull-right-container">
-            <i class="fa fa-angle-left pull-right"></i>
-          </span>
-        </a>
-        <ul class="treeview-menu">
-          <li><a href="#">Link in level 2</a></li>
-          <li><a href="#">Link in level 2</a></li>
-        </ul>
+      <a href="#"><i class="fa fa-user-secret"></i> <span>RBAC</span>
+      <span class="pull-right-container">
+      <i class="fa fa-angle-left pull-right"></i>
+      </span>
+      </a>
+      <ul class="treeview-menu">
+        <li><a href="#"><i class="fa fa-circle-o"></i>Roles</a></li>
+        <li><a href="#"><i class="fa fa-circle-o"></i>Permissions</a></li>
+      </ul>
       </li>
+      <li><a href="#"><i class="fa fa-users"></i> <span>Users</span></a></li>
+      <li><a href="#"><i class="fa fa-list"></i> <span>Posts</span></a></li>
+      <li><a href="#"><i class="fa fa-tag"></i> <span>Tags</span></a></li>
     </ul>
     <!-- /.sidebar-menu -->
   </section>
