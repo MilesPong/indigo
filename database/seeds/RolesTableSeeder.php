47158c807f930ca53989163886992e6c2af48e9f
Miles Pong <mingpeng16@gmail.com>
2017-05-13 22:21:01 +0800

    Add Role factories&seeder

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class RolesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        $this->call(RolesTableSeeder::class);
+        factory(\App\Models\Role::class, 10)->create();
     }
 }
