b80cd9b0fe269055b2e4b28821a62bd55669bb4c
Miles Pong <mingpeng16@gmail.com>
2017-05-27 15:02:49 +0000

    Add tags&taggables migration

---
+++ database/migrations/2017_05_27_224901_create_taggables_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTaggablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('taggables', function (Blueprint $table) {
+            $table->integer('tag_id')->unsigned()->index();
+            $table->morphs('taggable');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('taggables');
+    }
+}
