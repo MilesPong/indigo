f301d083e582453009fd6db702750f57e40e01f6
Miles Pong <mingpeng16@gmail.com>
2017-06-04 06:12:38 +0000

    Fix dismissible alert icon

--- resources/views/admin/components/dismissible.blade.php
+++ resources/views/admin/components/dismissible.blade.php
@@ -1,7 +1,7 @@
 <div class="alert alert-{{ $type }} alert-dismissible">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
     <h4>
-        <i class="icon fa fa-ban"></i>
+        <i class="icon fa fa-{{ $type == 'success' ? 'check' : 'ban' }}"></i>
         @if(isset($title))
             {{ ucfirst($title) }}
         @endif
