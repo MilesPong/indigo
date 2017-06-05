e1d41172a83ab3fd4238647ae05f04af9949148c
Miles Pong <mingpeng16@gmail.com>
2017-06-05 14:24:38 +0000

    Change text html_header_title to title

--- resources/views/admin/partials/html-header.blade.php
+++ resources/views/admin/partials/html-header.blade.php
@@ -1,7 +1,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>@yield('html_header_title', 'Dashboard') - {{ config('app.name', 'Laravel') }}</title>
+  <title>@yield('title', 'Dashboard') - {{ config('app.name', 'Laravel') }}</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
