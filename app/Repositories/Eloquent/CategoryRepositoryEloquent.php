3fa816bcf21ac31ede6f94757034790819d09557
Miles Pong <mingpeng16@gmail.com>
2017-06-13 14:47:54 +0800

    Add allWithPostCount contract&concrete in CateRepository

--- app/Repositories/Eloquent/CategoryRepositoryEloquent.php
+++ app/Repositories/Eloquent/CategoryRepositoryEloquent.php
@@ -5,6 +5,7 @@ namespace App\Repositories\Eloquent;
 use App\Models\Category;
 use App\Repositories\Contracts\CategoryRepository;
 use App\Repositories\Eloquent\Traits\Slugable;
+use App\Scopes\PublishedScope;
 
 /**
  * Class CategoryRepositoryEloquent
@@ -54,5 +55,24 @@ class CategoryRepositoryEloquent extends Repository implements CategoryRepositor
         $attributes = $this->preHandleData($attributes);
 
         return $this->update($attributes, $id);
+    }
+
+    /**
+     * @param array $columns
+     * @return mixed
+     */
+    public function allWithPostCount($columns = ['*'])
+    {
+        return $this->withCount([
+            'posts' => function ($query) {
+                if (isAdmin()) {
+                    $query->withoutGlobalScope(PublishedScope::class);
+                }
+            }
+        ])
+            ->all($columns)
+            ->reject(function ($category) {
+                return $category->posts_count == 0;
+            });
     }
 }
