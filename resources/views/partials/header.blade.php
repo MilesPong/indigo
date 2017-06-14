c1e0e46696cc5731afb623f47432d6b324a44fa2
Miles Pong <mingpeng16@gmail.com>
2017-06-14 15:02:15 +0800

    Add static avatar.jpg to header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,7 +2,7 @@
     <div class="section">
         <div class="container">
             <div class="row">
-                <img src="/images/me.jpg" alt="" class="col s4 offset-s4 m2 offset-m5 circle responsive-img">
+                <img src="{{ asset('images/avatar.jpg') }}" alt="" class="col s4 offset-s4 m2 offset-m5 circle responsive-img">
             </div>
             <div class="row center white-text">
                 <h5>Miles Peng</h5>
