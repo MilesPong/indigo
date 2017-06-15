52bcd57ba1d9b02f373a389334c33dd35fb8ced8
Miles Pong <mingpeng16@gmail.com>
2017-06-15 17:11:24 +0800

    Add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    @include('partials.favicon')
+
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     {{--<!--Import Google Icon Font-->--}}
