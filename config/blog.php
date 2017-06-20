5ee834ae60bd431b1edd8715c0c8e68d77ab7df5
Miles Pong <mingpeng16@gmail.com>
2017-06-20 11:49:43 +0000

    Update default config

--- config/blog.php
+++ config/blog.php
@@ -2,10 +2,11 @@
 
 return [
     'counter' => [
+        'strict_mode' => env('COUNT_STRICT_MODE', false),
+        'cache_key' => 'post_viewed_count:',
+        // require strict_mode to be set
         'timeout' => 3600, // 1h
         'key' => 'post_viewed',
-        'cache_key' => 'post_viewed_count:',
-        'strict_mode' => true,
     ],
     'analytics' => [
         'google_trace_id' => env('GOOGLE_ANALYTICS_ID'),
