d53788c4559d1e03c270353f1543b914ec5eca97
Miles Pong <mingpeng16@gmail.com>
2017-07-12 11:40:53 +0800

    Update css

--- resources/views/widgets/category.blade.php
+++ resources/views/widgets/category.blade.php
@@ -1,5 +1,5 @@
 <div class="collection with-header z-depth-1">
-    <div class="collection-header"><h5>Categories</h5></div>
+    <div class="collection-header grey lighten-4"><h5>Categories</h5></div>
     @foreach($categories as $category)
         <a href="{{ route('categories.show', $category->slug) }}" class="collection-item waves-effect black-text">
             <span class="badge" data-badge-caption="">{{ $category->posts_count }}</span>
--- resources/views/widgets/tag.blade.php
+++ resources/views/widgets/tag.blade.php
@@ -1,6 +1,6 @@
 <div class="collection with-header z-depth-1">
-    <div class="collection-header"><h5>Tags</h5></div>
-    <div class="tag-list">
+    <div class="collection-header grey lighten-4"><h5>Tags</h5></div>
+    <div class="tag-list white">
         @foreach($tags as $tag)
             <a class="chip waves-effect waves-teal" href="{{ route('tags.show', $tag->slug) }}">
                 {{ $tag->name }}
