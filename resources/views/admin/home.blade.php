e3066fe95b89314fd5b743c6823acc7d876041e9
Miles Pong <mingpeng16@gmail.com>
2017-12-26 22:52:35 +0800

    Add content in dashboard.home

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,5 +1,40 @@
 @extends('admin.layouts.app')
 
 @section('content')
-    <h1>Indigo Dashboard Home Page</h1>
+    <h4 class="center-align">Indigo Dashboard Home Page</h4>
+
+    <div class="row">
+        <div class="col s12 m6 l3">
+            <div class="card-panel teal">
+                <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+                I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+                </span>
+            </div>
+        </div>
+
+        <div class="col s12 m6 l3">
+            <div class="card-panel indigo">
+                <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+                I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+                </span>
+            </div>
+        </div>
+
+        <div class="col s12 m6 l3">
+            <div class="card-panel deep-orange">
+                <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+                I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+                </span>
+            </div>
+        </div>
+
+        <div class="col s12 m6 l3">
+            <div class="card-panel blue">
+                <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+                I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+                </span>
+            </div>
+        </div>
+    </div>
+    
 @endsection
