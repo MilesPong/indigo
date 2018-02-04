2bba51e385e882b2ac7da0d1306f3274010f6795
Miles Pong <mingpeng16@gmail.com>
2018-02-04 17:25:32 +0800

    Remove old Counter

--- config/blog.php
+++ config/blog.php
@@ -1,13 +1,6 @@
 <?php
 
 return [
-    'counter' => [
-        'strict_mode' => env('COUNT_STRICT_MODE', false),
-        'cache_key' => 'post_viewed_count:',
-        // require strict_mode to be set
-        'timeout' => 3600, // 1h
-        'key' => 'post_viewed',
-    ],
     'posts' => [
         'per_page' => 5,
     ],
