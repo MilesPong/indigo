9d1169b1f1227b68a7d8d28b5118604f3544e7d2
Miles Pong <mingpeng16@gmail.com>
2018-02-07 22:35:27 +0800

    Add google drive filesystem driver

--- config/app.php
+++ config/app.php
@@ -178,6 +178,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\ComposerServiceProvider::class,
         \App\Providers\CounterServiceProvider::class,
+        \App\Providers\GoogleDriveServiceProvider::class,
 
         // Rap2hpoutre\LaravelLogViewer\LaravelLogViewerServiceProvider::class,
         // Zizaco\Entrust\EntrustServiceProvider::class,
