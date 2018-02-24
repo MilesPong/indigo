1c5ebf6df21c63f30bb5805f4ef9fdc1f55fc3e5
Miles Pong <mingpeng16@gmail.com>
2018-02-24 11:06:31 +0800

    Update home page in dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,40 +1,57 @@
 @extends('admin.layouts.app')
 
 @section('content')
-    <h4 class="center-align">Indigo Dashboard Home Page</h4>
+    <h4 class="center-align">Home Page</h4>
 
-    <div class="row">
+    <div class="row stats">
         <div class="col s12 m6 l3">
-            <div class="card-panel teal">
-                <span class="white-text">I am a very simple card. I am good at containing small bits of information.
-                I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
-                </span>
-            </div>
+            <a href="{{ route('admin.posts.index') }}">
+                <div class="card-panel teal white-text">
+                    <h5>Posts</h5>
+                    <span>{{ $postCount }}</span>
+                </div>
+            </a>
         </div>
 
         <div class="col s12 m6 l3">
-            <div class="card-panel indigo">
-                <span class="white-text">I am a very simple card. I am good at containing small bits of information.
-                I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
-                </span>
-            </div>
+            <a href="{{ route('admin.pages.index') }}">
+                <div class="card-panel indigo white-text">
+                    <h5>Pages</h5>
+                    <span>{{ $pageCount }}</span>
+                </div>
+            </a>
         </div>
 
         <div class="col s12 m6 l3">
-            <div class="card-panel deep-orange">
-                <span class="white-text">I am a very simple card. I am good at containing small bits of information.
-                I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
-                </span>
-            </div>
+            <a href="{{ route('admin.categories.index') }}">
+                <div class="card-panel deep-orange white-text">
+                    <h5>Categories</h5>
+                    <span>{{ $categoryCount }}</span>
+                </div>
+            </a>
         </div>
 
         <div class="col s12 m6 l3">
-            <div class="card-panel blue">
-                <span class="white-text">I am a very simple card. I am good at containing small bits of information.
-                I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
-                </span>
-            </div>
+            <a href="{{ route('admin.tags.index') }}">
+                <div class="card-panel blue white-text">
+                    <h5>Tags</h5>
+                    <span>{{ $tagCount }}</span>
+                </div>
+            </a>
         </div>
+
     </div>
     
 @endsection
+
+@push('css')
+    <style>
+        .stats .card-panel {
+            display: flex;
+            display: -webkit-flex;
+            flex-direction: column;
+            align-items: center;
+            font-size: 1.8rem;
+        }
+    </style>
+@endpush
