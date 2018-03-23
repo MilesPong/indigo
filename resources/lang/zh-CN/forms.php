dff0ce6214d02f44ba3b8be12275390b50dedc35
Miles Pong <mingpeng16@gmail.com>
2018-03-23 15:20:37 +0800

    Add zh-CN localization support

---
+++ resources/lang/zh-CN/forms.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'actions' => [
+        'save' => '保存',
+        'create' => '创建',
+        'submit' => '提交',
+        'delete' => '删除',
+        'revert' => '恢复',
+        'delete_forever' => '永久删除',
+        'upload' => '上传',
+        'view' => '浏览',
+        'edit' => '编辑',
+        'new' => '新建',
+    ],
+
+    'action' => '操作',
+    'trash' => '回收站'
+];
